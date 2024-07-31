3109d70cfda29dbeb2f2e129bfc7ee68d8139b20
Mahedi Zaman Zaber <88625958+MahediZaber51@users.noreply.github.com>
2024-07-31 13:57:27 +0600

    3A solution.cpp
    The king is left alone on the chessboard. In spite of this loneliness, he doesn't lose heart, because he has business of national importance. For example, he has to pay an official visit to square t. As the king is not in habit of wasting his time, he wants to get from his current position s to square t in the least number of moves. Help him to do this.
    
    
    In one move the king can get to the square that has a common side or a common vertex with the square the king is currently in (generally there are 8 different squares he can move to).

---
+++ 3A - Shortest path of the king/solution.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    char source[3], destination[3];
+    cin >> source;
+    cin >> destination;
+
+    char letters[8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    int letterValues[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+
+    char steps[100][3];
+    int stepCount = 0;
+
+    int horizontal = letterValues[destination[0] - 'a'] - letterValues[source[0] - 'a'];
+    int vertical = destination[1] - source[1];
+
+    while (horizontal != 0 || vertical != 0)
+    {
+        if (horizontal < 0)
+        {
+            if (vertical < 0)
+            {
+                strcpy(steps[stepCount], "LD");
+                horizontal += 1;
+                vertical += 1;
+            }
+            else if (vertical > 0)
+            {
+                strcpy(steps[stepCount], "LU");
+                horizontal += 1;
+                vertical -= 1;
+            }
+            else
+            {
+                strcpy(steps[stepCount], "L");
+                horizontal += 1;
+            }
+        }
+        else if (horizontal > 0)
+        {
+            if (vertical < 0)
+            {
+                strcpy(steps[stepCount], "RD");
+                horizontal -= 1;
+                vertical += 1;
+            }
+            else if (vertical > 0)
+            {
+                strcpy(steps[stepCount], "RU");
+                horizontal -= 1;
+                vertical -= 1;
+            }
+            else
+            {
+                strcpy(steps[stepCount], "R");
+                horizontal -= 1;
+            }
+        }
+        else
+        {
+            if (vertical < 0)
+            {
+                strcpy(steps[stepCount], "D");
+                vertical += 1;
+            }
+            else if (vertical > 0)
+            {
+                strcpy(steps[stepCount], "U");
+                vertical -= 1;
+            }
+        }
+        stepCount++;
+    }
+    cout << stepCount<<endl;
+    for (int i = 0; i < stepCount; i++)
+    {
+        cout << steps[i] << endl;
+    }
+
+    return 0;
+}
