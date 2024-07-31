61c592a92d26466f83099764dc7062de0dbef10e
Mahedi Zaman Zaber <88625958+MahediZaber51@users.noreply.github.com>
2024-07-31 12:58:59 +0600

    Create solution.cpp

---
+++ 467A - George and Accommodation/solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n,r=0;
+    cin >> n;
+    while(n--)
+    {
+        int p,q;
+        cin >> p >> q;
+        if(q-p >=2)
+        {
+            r++;
+        }
+
+    }
+    cout << r;
+    return 0;
+}
