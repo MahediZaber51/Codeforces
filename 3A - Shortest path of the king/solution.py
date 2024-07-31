221a705a2b14361f88cd17a4ca9010faab40efae
Mahedi Zaman Zaber <88625958+MahediZaber51@users.noreply.github.com>
2024-07-31 13:58:08 +0600

    Create solution.py
    The king is left alone on the chessboard. In spite of this loneliness, he doesn't lose heart, because he has business of national importance. For example, he has to pay an official visit to square t. As the king is not in habit of wasting his time, he wants to get from his current position s to square t in the least number of moves. Help him to do this.
    
    
    In one move the king can get to the square that has a common side or a common vertex with the square the king is currently in (generally there are 8 different squares he can move to).

---
+++ 3A - Shortest path of the king/solution.py
@@ -0,0 +1,44 @@
+source = list(input())
+destination = list(input())
+letters = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8}
+steps = []
+
+horizontal = letters[destination[0]] - letters[source[0]]
+vertical = int(destination[1]) - int(source[1])
+
+while horizontal != 0 or vertical != 0:
+  if horizontal < 0:
+    if vertical < 0:
+      steps.append("LD")
+      horizontal += 1
+      vertical += 1
+    elif vertical > 0:
+      steps.append("LU")
+      horizontal += 1
+      vertical -= 1
+    else:
+      steps.append("L")
+      horizontal += 1
+  elif horizontal > 0:
+    if vertical < 0:
+      steps.append("RD")
+      horizontal -= 1
+      vertical += 1
+    elif vertical > 0:
+      steps.append("RU")
+      horizontal -= 1
+      vertical -= 1
+    else:
+      steps.append("R")
+      horizontal -= 1
+  else:
+    if vertical < 0:
+      steps.append("D")
+      vertical += 1
+    elif vertical > 0:
+      steps.append("U")
+      vertical -= 1
+
+print(len(steps))
+for i in steps:
+  print(i)
